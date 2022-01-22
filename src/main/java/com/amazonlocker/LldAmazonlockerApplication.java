1d648ef5b5680d73d52a4e1eb5d7a05c487a5657
praveen-raj <praveen.raj@mettl.com>
2022-01-22 19:13:04 +0530

    Initial commit

---
+++ src/main/java/com/amazonlocker/LldAmazonlockerApplication.java
@@ -0,0 +1,13 @@
+package com.amazonlocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldAmazonlockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldAmazonlockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amazonlocker/LldAmazonlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonlocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldAmazonlockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
